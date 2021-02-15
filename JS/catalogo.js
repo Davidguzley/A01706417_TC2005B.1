282a549695ff830e0e02235a9a30bf7293a6c6f3
Davidguzley <A01706417@itesm.mx>
2021-02-15 13:40:07 -0600

    Lab5 terminado y Bootstrap implementado

--- JS/catalogo.js
+++ JS/catalogo.js
@@ -1,27 +1,17 @@
-class Producto{
-    constructor(precio){
-        this.precio = precio
-    }
-    
-    costoPieza(n){
-        let total = n*this.precio;
-        return total;
-    }
+function añadir(){
+        var telefonos = document.getElementById("n_telefono");
+        var guantes = document.getElementById("n_guante");
+        var jordans = document.getElementById("n_jordan");
 
-    //Calcula los impuestos pagados por el articulo
-    impuestos(cant){
-        const IVA = 0.16*cant;
-        return (IVA);
-    }
+        var c_telefonos = telefonos.value * 15999;
+        var c_guantes = guantes.value * 2500;
+        var c_jordans = jordans.value * 4850;
+        var total = c_telefonos + c_guantes + c_jordans;
+
+        let resultado = document.getElementById("fin");
+        resultado.innerHTML = `<br><br><h3> Total: ${total} IVA: ${total*0.16} </h3> <button id= "pago" onclick="ordenar()">Confirmar compra</button>`;
 }
 
-const telefono = new Producto(15999);
-const guante = new Producto(2500);
-const jordan = new Producto(4850);
-
-function comprar(){
-    let boton =  document.getElementById("comprar");
-    boton.onclick = () => {
-        let cantidad = document.getElementById("cant").value;
-    }
+function ordenar(){
+    alert("¡¡Muchas gracias por tu compra!! :)");
 }
