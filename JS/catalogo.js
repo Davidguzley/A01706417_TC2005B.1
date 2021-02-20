f8e2a300975d4c201ef1a9c53faf406ebc17967c
Davidguzley <A01706417@itesm.mx>
2021-02-20 16:32:59 -0600

    Lab 6: Frameworks de estilo

--- JS/catalogo.js
+++ JS/catalogo.js
@@ -9,9 +9,10 @@ function añadir(){
         var total = c_telefonos + c_guantes + c_jordans;
 
         let resultado = document.getElementById("fin");
-        resultado.innerHTML = `<br><br><h3> Total: ${total} IVA: ${total*0.16} </h3> <button id= "pago" onclick="ordenar()">Confirmar compra</button>`;
+        resultado.innerHTML = `<br><br><h3 id= "alerta"> Total: ${total} IVA: ${total*0.16} </h3> <button onclick="ordenar()">Confirmar compra</button>`;
 }
 
 function ordenar(){
     alert("¡¡Muchas gracias por tu compra!! :)");
+    document.getElementById("alerta").innerHTML = `<span class="badge rounded-pill bg-success">Success</span>`;
 }
