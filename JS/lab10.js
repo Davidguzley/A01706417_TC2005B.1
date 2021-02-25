82d8f3d320206ca4d8780f79f1360eab0c0c5f71
Davidguzley <A01706417@itesm.mx>
2021-02-25 14:26:23 -0600

    lab10 terminado

--- JS/lab10.js
+++ JS/lab10.js
@@ -1,25 +1,26 @@
 const http = require('http');
+const filesystem = require('fs');
+
+class EVA{
+    constructor(nombre, children, pais){
+        this.nombre = nombre;
+        this.children = children;
+        this.pais = pais;
+    }
+
+    toString(){
+        return (`Unidad EVA ${this.nombre} pilotada por ${this.children} de ${this.pais}.`);
+    }
+}
+
+const unidad1 = new EVA("00", "Rei Ayanami", "Japón");
+const unidad2 = new EVA("01", "Shinji Ikari", "Japón");
+const unidad3 = new EVA("02", "Asuka Langley", "Alemania");
+const EVAS = [unidad1, unidad2, unidad3];
 
 const server = http.createServer( (request, response) => {
     console.log("hola desde el servidor");
     console.log(request.url);
-
-    class EVA{
-        constructor(nombre, children, pais){
-            this.nombre = nombre;
-            this.children = children;
-            this.pais = pais;
-        }
-    
-        toString(){
-            return (`Unidad ${this.nombre} pilotada por ${this.children} de ${this.pais}.`);
-        }
-    }
-
-    const unidad1 = new EVA("EVA 00", "Rei Ayanami", "Japón");
-    const unidad2 = new EVA("EVA 01", "Shinji Ikari", "Japón");
-    const unidad3 = new EVA("EVA 02", "Asuka Langley", "Alemania");
-    const EVAS = [unidad1, unidad2, unidad3];
     
     if (request.url === "/unidades") {
         response.setHeader('Content-Type', 'text/html');
@@ -48,9 +49,9 @@ const server = http.createServer( (request, response) => {
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>NERV</title></head>');
         response.write("<body><h1>Asigna una nueva unidad Evangelion</h1>");
-        response.write('<form action="nueva-unidad" method="POST"> <input type="text" name="nombre"> <br>');
-        response.write('<input type="text" name="children"> <br>');
-        response.write('<input type="text" name="pais"> <br> <input type="submit" value="Guardar EVA"></form>');
+        response.write('<form action="nueva-unidad" method="POST"> <input type="text" name="nombre" placeholder="Numero de EVA"> <br>');
+        response.write('<input type="text" name="children" placeholder="Nombre del niño elegido"> <br>');
+        response.write('<input type="text" name="pais" placeholder="Pais de origen del niño"> <br> <input type="submit" value="Guardar EVA"></form>');
         response.write("</body>");
         response.write("</html>");
         response.end();
@@ -65,23 +66,21 @@ const server = http.createServer( (request, response) => {
         return request.on('end', () => {
             const datos_completos = Buffer.concat(datos).toString();
             console.log(datos_completos);
-            const eq1 = datos_completos.indexOf("=") + 1;
-            console.log(eq1);
-            const eq2 = datos_completos.indexOf("=", eq1) +1;
-            console.log(eq2);
-            const eq3 = datos_completos.indexOf('=', eq2) + 1;
-            console.log(eq3);
-            const and1 = datos_completos.indexOf("&") + 1;
-            console.log(and1);
-            const and2 = datos_completos.indexOf("&", and1) + 1;
-            console.log(and2);
-            const nombre = datos_completos.substr(eq1, and1);
-            const niño = datos_completos.substr(eq2, and2);
-            const pais = datos_completos.substr(eq3);
-            const nueva_unidad = new EVA(nombre,niño,pais)
+            const entrada = datos_completos.replace(/[+]/gi, ' ');
+            console.log(entrada);
+            const eq1 = entrada.indexOf("=") + 1;
+            const eq2 = entrada.indexOf("=", eq1) + 1;
+            const eq3 = entrada.indexOf('=', eq2) + 1;
+            const and1 = entrada.indexOf("&") + 1;
+            const and2 = entrada.indexOf("&", and1) + 1;
+            const nombre = entrada.substr(eq1, (and1-eq1)-1);
+            const niño = entrada.substr(eq2, (and2-eq2)-1);
+            const pais = entrada.substr(eq3);
+            var nueva_unidad = new EVA(nombre, niño, pais)
+            filesystem.writeFileSync('lab10.txt', nueva_unidad.toString());
             EVAS.push(nueva_unidad);
-            console.log(nueva_unidad.toString());
-            console.log(EVAS);
+            response.statusCode = 302;
+            response.setHeader('Location', '/unidades');
             return response.end();
         });
 
@@ -91,7 +90,11 @@ const server = http.createServer( (request, response) => {
         response.setHeader('Content-Type', 'text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>NERV</title></head>');
-        response.write("<body><h1>Hola desde el servidor</h1></body>");
+        response.write("<body><h1>BIENVENIDO A NERV</h1>");
+        response.write('<h2>¿Quienes somos?</h2>');
+        response.write('<p>Somos una organización paramilitar dependiente de la ONU. Dedicada en cuerpo y alma a la reconstrucción y protección del mundo, con la función de estudiar y eliminar a los Ángeles. En relación a la cadena de mando, nuestro comandante Gendō Ikari, junto con el vice-comandante Kōzō Fuyutsuki, son quienes dirigen casi en su totalidad la organización, aunque en realidad los verdaderos jefes de NERV son los miembros de la organización SEELE, con Keel Lorentz a la cabeza.</p>');
+        response.write('<h2>Localización</h2>');
+        response.write('<p>Estamos localizados en el Geo-Frente (especie de caverna esférica subterránea de la cual el 89% está bajo tierra) de Tokio-3</p></body>');
         response.write("</html>");
         response.end();
     }
@@ -101,7 +104,7 @@ const server = http.createServer( (request, response) => {
         response.setHeader('Content-Type', 'text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>Page not found</title></head>');
-        response.write("<body><h1>Page no encontrada en la base de datos de NERV.</h1></body>");
+        response.write("<body><h1>Pagina no encontrada en la base de datos de NERV.</h1></body>");
         response.write("</html>");
         return response.end();
     }
